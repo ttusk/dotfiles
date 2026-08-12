59f55c4b0ae16a53908f26d8a0c741df2f3cc42c
Luiz Gustavo <luizgust.x@gmail.com>
2026-08-12 19:44:32 -0300

    feat(nvim): add typescript solidjs support

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -20,6 +20,35 @@ vim.lsp.config["lua_ls"] = {
     },
 }
 
+vim.lsp.config["ts_ls"] = {
+    cmd = { "typescript-language-server", "--stdio" },
+    filetypes = {
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+    },
+    root_markers = {
+        { "tsconfig.json", "jsconfig.json" },
+        "package.json",
+        ".git",
+    },
+    -- SolidJS uses TypeScript's standard TSX language service. A project's
+    -- tsconfig.json should set jsxImportSource to "solid-js".
+    init_options = {
+        hostInfo = "neovim",
+        preferences = {
+            includeCompletionsForModuleExports = true,
+            includeCompletionsWithSnippetText = true,
+            jsxAttributeCompletionStyle = "braces",
+        },
+    },
+}
+
 if vim.fn.executable("lua-language-server") == 1 then
     vim.lsp.enable("lua_ls")
+end
+
+if vim.fn.executable("typescript-language-server") == 1 then
+    vim.lsp.enable("ts_ls")
 end
