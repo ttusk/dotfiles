c7f354b45ca9917d2297894d355364ca55470161
Luiz Gustavo <luizgust.x@gmail.com>
2026-08-12 19:25:30 -0300

    feat(nvim): adapt theme to system appearance

--- .config/nvim/lua/config/theme.lua
+++ .config/nvim/lua/config/theme.lua
@@ -1,7 +1,20 @@
 local opt = vim.opt
 
 opt.termguicolors = true
-opt.background = "light"
+
+local function system_background()
+    if vim.fn.has("macunix") == 1 or vim.fn.has("mac") == 1 then
+        local style = vim.fn.system({ "defaults", "read", "-g", "AppleInterfaceStyle" })
+
+        if style:lower():match("dark") then
+            return "dark"
+        end
+    end
+
+    return "light"
+end
+
+opt.background = system_background()
 
 require("solarized").setup({})
 vim.cmd.colorscheme("solarized")
