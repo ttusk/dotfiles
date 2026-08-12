fa9e4790420d905420c6bcb8098af445ceb53b76
Luiz Gustavo <luizgust.x@gmail.com>
2026-08-12 19:44:26 -0300

    feat(nvim): configure rust analyzer

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -23,3 +23,17 @@ vim.lsp.config["lua_ls"] = {
 if vim.fn.executable("lua-language-server") == 1 then
     vim.lsp.enable("lua_ls")
 end
+
+vim.lsp.config["rust_analyzer"] = {
+    cmd = { "rust-analyzer" },
+    filetypes = { "rust" },
+    root_markers = {
+        "Cargo.toml",
+        "rust-project.json",
+        ".git",
+    },
+}
+
+if vim.fn.executable("rust-analyzer") == 1 then
+    vim.lsp.enable("rust_analyzer")
+end
